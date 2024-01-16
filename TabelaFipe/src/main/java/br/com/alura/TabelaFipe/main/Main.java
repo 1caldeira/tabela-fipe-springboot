4975e844d0ee32008da895310c974c993c21b1a8
1caldeira <g.caldeiraa1@gmail.com>
2024-01-16 15:57:05 -0300

    Melhorias

--- TabelaFipe/src/main/java/br/com/alura/TabelaFipe/main/Main.java
+++ TabelaFipe/src/main/java/br/com/alura/TabelaFipe/main/Main.java
@@ -2,8 +2,11 @@ package br.com.alura.TabelaFipe.main;
 
 import br.com.alura.TabelaFipe.model.*;
 import br.com.alura.TabelaFipe.service.RequestAPI;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -15,34 +18,41 @@ public class Main {
         System.out.println("Bem-vindo ao sistema de pesquisa da tabela FIPE");
         System.out.println("Deseja pesquisar por 'carros', 'motos' ou 'caminhoes?'");
         Scanner sc = new Scanner(System.in);
-        String search = sc.nextLine();
+        String vehicleType = sc.nextLine();
 
-        var json = requestAPI.getData(ADDRESS + search + "/marcas");
+        var json = requestAPI.getData(ADDRESS + vehicleType + "/marcas");
         List<VehicleTypeData> vehicleTypeDataList = conversion.getDataList(json, VehicleTypeData.class);
         vehicleTypeDataList.forEach(v -> System.out.println("Marca: " + v.brand() + " ---  Codigo: " + v.code()));
 
         System.out.println("Digite o codigo da marca que deseja pesquisar: ");
         int brandCode = sc.nextInt();
-        json = requestAPI.getData(ADDRESS + search + "/marcas/" + brandCode + "/modelos");
+        json = requestAPI.getData(ADDRESS + vehicleType + "/marcas/" + brandCode + "/modelos");
         BrandData brandData = conversion.getData(json, BrandData.class);
         for (ModelDataInfo m : brandData.modelDataInfo()) {
             System.out.println("Modelo: "+m.modelName()+" --- Codigo: "+m.code());
         }
-        System.out.println("Escolha o modelo que deseja pesquisar por meio do codigo pertencente a ele: ");
-        int modelCode = sc.nextInt();
-        json = requestAPI.getData(ADDRESS + search + "/marcas/" + brandCode + "/modelos/"+modelCode+"/anos");
-        List<YearData> yearDataList = conversion.getDataList(json, YearData.class);
-        for (YearData yd:yearDataList
-             ) {
-            System.out.println("Ano/Tipo Combustivel: "+yd.yearAndFuel()+" ------- Codigo: "+yd.code());
-        }
-        System.out.println("Escolha o modelo desejado por meio de seu codigo: ");
-        sc.nextLine();
-        String yearCode = sc.nextLine();
-        json = requestAPI.getData(ADDRESS + search + "/marcas/" + brandCode + "/modelos/"+modelCode+"/anos/"+yearCode);
-        ModelData modelData = conversion.getData(json, ModelData.class);
-        System.out.println("Nome do modelo: "+modelData.model()+"\nMarca: "+modelData.brandName()+"\nAno: "+modelData.year()+"\n" +
-                "Valor: "+modelData.value());
 
+        System.out.println("Digite um trecho do nome do modelo que deseja pesquisar: ");
+        sc.nextLine();
+        String searchModel = sc.nextLine();
+        Stream <ModelDataInfo> research = brandData.modelDataInfo().stream()
+                .filter(m -> m.modelName().toUpperCase().contains(searchModel.toUpperCase()));
+
+        research.forEach(System.out::println);
+
+        System.out.println("Qual o codigo do modelo que deseja visualizar? ");
+        int modelCode = sc.nextInt();
+        json = requestAPI.getData(ADDRESS + vehicleType + "/marcas/" + brandCode + "/modelos/"+modelCode+"/anos");
+        List<YearData> yearDataList = conversion.getDataList(json, YearData.class);
+        List<String> yearCodes = new ArrayList<>();
+        for (YearData yd:yearDataList) {
+            yearCodes.add(yd.code());
         }
+        for (String yearCode:yearCodes) {
+            json = requestAPI.getData(ADDRESS + vehicleType + "/marcas/" + brandCode + "/modelos/"+modelCode+"/anos/"+yearCode);
+            ModelData modelData = conversion.getData(json, ModelData.class);
+            System.out.println(modelData.model()+"  Ano: "+modelData.year()+"  Valor: "+modelData.value()+"  Combustivel: "+modelData.fuelType());
+        }
+
+    }
 }
--- TabelaFipe/src/main/java/br/com/alura/TabelaFipe/model/ModelDataInfo.java
+++ TabelaFipe/src/main/java/br/com/alura/TabelaFipe/model/ModelDataInfo.java
@@ -6,4 +6,9 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record ModelDataInfo(@JsonAlias("codigo") Integer code,
                             @JsonAlias("nome") String modelName) {
+
+    @Override
+    public String toString() {
+        return "Modelo: "+modelName+" --- Codigo: "+code;
+    }
 }
